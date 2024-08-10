20a24f4d75439eff3460c96ffc39c82b679a5340
AsifAleem-del <asifaleemkhan@gmail.com>
2024-08-10 13:15:51 +0530

    changes implemented on smart scroll

--- js/main.js
+++ js/main.js
@@ -16,3 +16,20 @@ function topFunction(){
   function toggleNav(x) {
     x.classList.toggle("active");
   }
+
+  // Reveal Website Elements On Scroll
+  function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i = 0; i < reveals.length; i++){
+      var windowheight = window.innerHeight;
+      var revealtop = reveals[i].getBoundingClientRect().top;
+      var revealpoint = 150;
+      if (revealtop < windowheight - revealpoint){
+        reveals[i].classList.add('active');
+      }
+      else{
+        reveals[i].classList.remove('active');
+      }
+    }
+  }
+  window.addEventListener('scroll', reveal);

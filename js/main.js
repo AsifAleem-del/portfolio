b07f51795330718458a3ad4551e63797406727b5
AsifAleem-del <asifaleemkhan@gmail.com>
2024-08-11 14:01:13 +0530

    Fix Some UI issues and update contect

--- js/main.js
+++ js/main.js
@@ -6,6 +6,17 @@ var typed = new Typed('#text', {
     loop:true,
 });
 
+// scroll top coding start from here
+let myButton = document.getElementById("myBtn");
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+        function scrollFunction(){
+          if (document.bodyscrollTop > 20 || document.documentElement.scrollTop > 20 ){
+            myButton.style.display =  "block";
+          } else {
+            myButton.style.display = "none";
+          }
+        }
 // When the user clicks on the button, scroll to the top of the document
 function topFunction(){
     document.body.scrollTop = 0;

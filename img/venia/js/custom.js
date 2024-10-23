5c6e62bd8b2d93e95571f0b5ba3974942c752a04
AsifAleem-del <asifaleemkhan@gmail.com>
2024-10-23 20:01:15 +0530

    Add venia website

---
+++ img/venia/js/custom.js
@@ -0,0 +1,31 @@
+fetch('https://fakestoreapi.com/products')
+.then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    data.forEach(user => {
+       const image = `<img src="${user.image}"/>`;
+       const title = `<h2>${user.title}</h2>`;
+       const category = `<p>${user.category}</p>`;
+       const price = `<p>${user.price}</p>`;
+       const description = `<h5>${user.description}</h5>`;
+       const rating = `<span>${user.rating.rate}</span>`;
+       const container =`<div><img src="${user.image}"/><h2>${user.title}</h2><p> ${user.price}</p><span class="icon-container"><i class="fa-regular fa-heart"></i><span> ${user.rating.rate}</span></span></div>`;
+       document.querySelector('aside').insertAdjacentHTML('beforeend', container);
+    });
+    console.log(data);
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+   // Mobile view Navigation toggle code start from here
+   function toggleNav(x) {
+    x.classList.toggle("active");
+  }
+  function removeActive() {
+    var element = document.getElementById("iconToggle");
+    element.classList.remove("active");
+  }

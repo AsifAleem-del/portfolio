53870937e9f6c63991afd291794716e57250ce2e
AsifAleem-del <asifaleemkhan@gmail.com>
2024-10-28 13:38:17 +0530

    Add loading icon on page load

--- img/venia/js/custom.js
+++ img/venia/js/custom.js
@@ -8,13 +8,18 @@ fetch('https://fakestoreapi.com/products')
   })
   .then(data => {
     data.forEach(user => {
+      const loader = document.querySelector(".loader");
+      loader.classList.add("loader-hidden");
+      loader.addEventListener("transitionend", () =>{
+        document.body.removeChild("loader")
+      })
        const image = `<img src="${user.image}"/>`;
        const title = `<h2>${user.title}</h2>`;
        const category = `<p>${user.category}</p>`;
        const price = `<p>${user.price}</p>`;
        const description = `<h5>${user.description}</h5>`;
        const rating = `<span>${user.rating.rate}</span>`;
-       const container =`<div><img src="${user.image}"/><h2>${user.title}</h2><p> ${user.price}</p><span class="icon-container"><i class="fa-regular fa-heart"></i><span> ${user.rating.rate}</span></span></div>`;
+       const container =`<div><img src="${user.image}" loading="lazy"/><h2>${user.title}</h2><p> ${user.price}</p><span class="icon-container"><i class="fa-regular fa-heart"></i><span> ${user.rating.rate}</span></span></div>`;
        document.querySelector('aside').insertAdjacentHTML('beforeend', container);
     });
     console.log(data);
